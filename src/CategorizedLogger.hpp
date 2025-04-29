844db2a3f11f7ecdc25dfc8ce1d004ee2a0ba4ae
brano <branoverv10@bk.ru>
2025-04-29 23:47:19 +0300

    Make logger template for enum classes

--- src/CategorizedLogger.hpp
+++ src/CategorizedLogger.hpp
@@ -9,50 +9,56 @@
 
 namespace logger {
 
-enum LoggerName : uint8_t
-{
-    Core  = 0,
-    Count = 255
-};
-
-inline std::string toString(LoggerName name)
-{
-    switch (name)
-    {
-    case LoggerName::Core: return "Core";
-    case LoggerName::Count: return "Unknown";
-    }
-    return "Unknown";
-}
-
+template <class T>
 class CategorizedLogger
 {
+private:
+    using Category     = T;
+    using BaseCategory = typename Category::baseType;
+
 public:
     CategorizedLogger()
     {
         quill::Backend::start();
 
-        for (uint8_t i = 0; i < LoggerName::Count; ++i)
+        for (BaseCategory i = 0; i < Category::kCount; ++i)
         {
-            s_loggers[i] = quill::Frontend::create_or_get_logger(toString(static_cast<LoggerName>(i)),
-                quill::Frontend::create_or_get_sink<quill::ConsoleSink>(toString(static_cast<LoggerName>(i))));
-            s_loggers[i]->init_backtrace(32);
-            s_loggers[i]->set_log_level(quill::LogLevel::TraceL3);
+            m_loggers[i] = quill::Frontend::create_or_get_logger(Category::to_string(i).data(),
+                quill::Frontend::create_or_get_sink<quill::ConsoleSink>(Category::to_string(i).data()));
+            m_loggers[i]->init_backtrace(32);
+            m_loggers[i]->set_log_level(quill::LogLevel::TraceL3);
         }
     }
 
-    quill::Logger* getLogger(const LoggerName name)
+    quill::Logger* getLogger(const BaseCategory name)
     {
-        return s_loggers[name];
+        return m_loggers[name];
     }
 
 private:
-    std::array<quill::Logger*, LoggerName::Count> s_loggers;
+    std::array<quill::Logger*, Category::kCount> m_loggers;
 };
 }  // namespace logger
 
-#define DEFINE_CAT_LOGGER_MODULE(LoggerName) static logger::CategorizedLogger s_##LoggerName##Logger
+#define DEFINE_CAT_LOGGER_MODULE(LoggerName, CategoryType) static logger::CategorizedLogger<CategoryType> s_##LoggerName##Logger
 
-#define GET_LOGGER(LoggerName, name) logger::s_##LoggerName##Logger.getLogger(logger::name)
+#define GET_LOGGER(LoggerName, name, catName) logger::s_##LoggerName##Logger.getLogger(logger::catName::k##name)
+
+#define CAT_LOG_TRACE_L3(logName, catName, cat, message, ...) \
+    QUILL_LOG_TRACE_L3(GET_LOGGER(logName, cat, catName), message, ##__VA_ARGS__)
+#define CAT_LOG_TRACE_L2(logName, catName, cat, message, ...) \
+    QUILL_LOG_TRACE_L2(GET_LOGGER(logName, cat, catName), message, ##__VA_ARGS__)
+#define CAT_LOG_TRACE_L1(logName, catName, cat, message, ...) \
+    QUILL_LOG_TRACE_L1(GET_LOGGER(logName, cat, catName), message, ##__VA_ARGS__)
+#define CAT_LOG_DEBUG(logName, catName, cat, message, ...) \
+    QUILL_LOG_DEBUG(GET_LOGGER(logName, cat, catName), message, ##__VA_ARGS__)
+#define CAT_LOG_INFO(logName, catName, cat, message, ...) \
+    QUILL_LOG_INFO(GET_LOGGER(logName, cat, catName), message, ##__VA_ARGS__)
+#define CAT_LOG_WARNING(logName, catName, cat, message, ...) \
+    QUILL_LOG_WARNING(GET_LOGGER(logName, cat, catName), message, ##__VA_ARGS__)
+#define CAT_LOG_ERROR(logName, catName, cat, message, ...) \
+    QUILL_LOG_ERROR(GET_LOGGER(logName, cat, catName), message, ##__VA_ARGS__)
+#define CAT_LOG_CRITICAL(logName, catName, cat, message, ...) \
+    QUILL_LOG_CRITICAL(GET_LOGGER(logName, cat, catName), message, ##__VA_ARGS__)
 
 #endif  // LOGGER_CORE_HPP
