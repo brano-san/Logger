d8cfd13c9a5bd0d697ae27660a63a5bd0ed6372f
brano <branoverv10@bk.ru>
2025-05-03 17:07:32 +0300

    Add log level set to sinks from file

--- src/CategorizedLogger.hpp
+++ src/CategorizedLogger.hpp
@@ -1,17 +1,16 @@
 ﻿#ifndef LOGGER_CORE_HPP
 #define LOGGER_CORE_HPP
 
-#include "quill/Backend.h"
-#include "quill/Frontend.h"
-#include "quill/LogMacros.h"
-#include "quill/Logger.h"
+#include <print>
 
-#include "quill/sinks/ConsoleSink.h"
-#include "quill/sinks/RotatingFileSink.h"
+#include <quill/Logger.h>
+#include <quill/Backend.h>
+#include <quill/Frontend.h>
+#include <quill/LogMacros.h>
+#include <quill/sinks/FileSink.h>
+#include <quill/sinks/ConsoleSink.h>
 
 #include "SimpleIni.hpp"
-
-#include <print>
 
 namespace logger {
 
@@ -22,6 +21,20 @@ private:
     using Category     = T;
     using BaseCategory = typename Category::baseType;
 
+    struct SinkLogLevel
+    {
+        std::string_view sink;
+        std::string logLevel;
+    };
+
+    struct SinksLogLevel
+    {
+        std::unordered_map<std::string_view, std::string> logLevels = {
+            {std::string_view{"File"}, std::string{"T3"}},
+            {std::string_view{"Console"}, std::string{"I"}}
+        };
+    };
+
 public:
     CategorizedLogger()
     {
@@ -29,37 +42,29 @@ public:
 
         quill::Backend::start();
 
-        // File loggers
         for (BaseCategory i = 0; i < Category::kCount; ++i)
         {
             std::vector<std::shared_ptr<quill::Sink>> sinks;
 
-            // auto sink = quill::Frontend::create_or_get_sink<quill::RotatingFileSink>("log.txt",
-            //    []()
-            //    {
-            //        quill::RotatingFileSinkConfig cfg;
-            //        cfg.set_rotation_max_file_size(1024);
-            //        return cfg;
-            //    }());
+            // File Sink
+            quill::FileSinkConfig cfg;
+            cfg.set_open_mode('w');
+            cfg.set_filename_append_option(quill::FilenameAppendOption::StartCustomTimestampFormat, "_%d_%m_%Y_%H_%M_%S");
 
-            auto fileSink = quill::Frontend::create_or_get_sink<quill::FileSink>("logs/log.txt",
-                []()
-                {
-                    quill::FileSinkConfig cfg;
-                    cfg.set_open_mode('w');
-                    cfg.set_filename_append_option(quill::FilenameAppendOption::StartCustomTimestampFormat, "_%d_%m_%Y_%H_%M_%S");
-                    return cfg;
-                }());
-            fileSink->set_log_level_filter(quill::LogLevel::Info);
+            auto fileSink = quill::Frontend::create_or_get_sink<quill::FileSink>("logs/log.txt", std::move(cfg));
+            fileSink->set_log_level_filter(getLogLevelByShortName(s_loggerSinks[i].logLevels["File"]));
             sinks.push_back(fileSink);
 
+            // Console Sink
             auto consoleSink = quill::Frontend::create_or_get_sink<quill::ConsoleSink>(Category::to_string(i).data());
-            consoleSink->set_log_level_filter(quill::LogLevel::Error);
+            consoleSink->set_log_level_filter(getLogLevelByShortName(s_loggerSinks[i].logLevels["Console"]));
             sinks.push_back(consoleSink);
 
+            // Logger create
             m_loggers[i] = quill::Frontend::create_or_get_logger(Category::to_string(i).data(), std::move(sinks),
                 quill::PatternFormatterOptions{kPatternFormatterLogs.data(), kPatternFormatterTime.data()});
             m_loggers[i]->init_backtrace(32, quill::LogLevel::Critical);
+            m_loggers[i]->set_log_level(quill::LogLevel::TraceL3);
         }
     }
 
@@ -73,26 +78,45 @@ private:
     {
         CSimpleIniA loggerSettingsFile;
         loggerSettingsFile.LoadFile(kLoggerSettingsFileName.data());
+
+        quill::BackendOptions opt;
+        static_assert(opt.log_level_descriptions.size() == opt.log_level_short_codes.size());
+        for (uint32_t i = 0; i < opt.log_level_short_codes.size(); ++i)
+        {
+            loggerSettingsFile.SetValue("Description", opt.log_level_descriptions[i].data(), opt.log_level_short_codes[i].data());
+        }
+
         for (BaseCategory i = 0; i < Category::kCount; ++i)
         {
-            for (const auto sink : kLoggerSinks)
+            for (auto& sink : s_loggerSinks[i].logLevels)
             {
-                std::string_view a = loggerSettingsFile.GetValue(Category::to_string(i).data(), sink.data(), "I");
-                std::println("{} logger category set to \"{}\" level", Category::to_string(i).data(), a);
-
-                loggerSettingsFile.SetValue(Category::to_string(i).data(), sink.data(), a.data());
+                sink.second = loggerSettingsFile.GetValue(Category::to_string(i).data(), sink.first.data(), sink.second.data());
+                loggerSettingsFile.SetValue(Category::to_string(i).data(), sink.first.data(), sink.second.data());
             }
         }
+
         loggerSettingsFile.SaveFile(kLoggerSettingsFileName.data());
     }
 
-    static constexpr std::array kLoggerSinks = {std::string_view{"File"}, std::string_view{"Console"}};
+    static quill::LogLevel getLogLevelByShortName(std::string_view logLevel)
+    {
+        quill::BackendOptions opt;
+        auto it = std::find(opt.log_level_short_codes.begin(), opt.log_level_short_codes.end(), logLevel);
+        if (it == opt.log_level_short_codes.end())
+        {
+            std::println("Cannot find log level by given short name - {}. Return INFO", logLevel);
+            return quill::LogLevel::Info;
+        }
+        return static_cast<quill::LogLevel>(std::distance(opt.log_level_short_codes.begin(), it));
+    }
 
     static constexpr std::string_view kLoggerSettingsFileName = "LogSettings.ini";
 
     static constexpr std::string_view kPatternFormatterTime = "%H:%M:%S.%Qns";
     static constexpr std::string_view kPatternFormatterLogs =
         "[%(time)] [%(thread_id)] [%(short_source_location:^28)] [%(log_level:^11)] [%(logger:^6)] %(message)";
+
+    inline static std::array<SinksLogLevel, Category::kCount> s_loggerSinks;
 
     std::array<quill::Logger*, Category::kCount> m_loggers;
 };
@@ -103,7 +127,6 @@ private:
 
 #define GET_LOGGER(LoggerName, name, catName) logger::s_##LoggerName##Logger.getLogger(logger::catName::k##name)
 
-// clang-format off
 #define CAT_LOG_TRACE_L3(logName, catName, cat, message, ...) QUILL_LOG_TRACE_L3(GET_LOGGER(logName, cat, catName), message, ##__VA_ARGS__)
 #define CAT_LOG_TRACE_L2(logName, catName, cat, message, ...) QUILL_LOG_TRACE_L2(GET_LOGGER(logName, cat, catName), message, ##__VA_ARGS__)
 #define CAT_LOG_TRACE_L1(logName, catName, cat, message, ...) QUILL_LOG_TRACE_L1(GET_LOGGER(logName, cat, catName), message, ##__VA_ARGS__)
@@ -112,6 +135,5 @@ private:
 #define CAT_LOG_WARNING(logName, catName, cat, message, ...)  QUILL_LOG_WARNING(GET_LOGGER(logName, cat, catName), message, ##__VA_ARGS__)
 #define CAT_LOG_ERROR(logName, catName, cat, message, ...)    QUILL_LOG_ERROR(GET_LOGGER(logName, cat, catName), message, ##__VA_ARGS__)
 #define CAT_LOG_CRITICAL(logName, catName, cat, message, ...) QUILL_LOG_CRITICAL(GET_LOGGER(logName, cat, catName), message, ##__VA_ARGS__)
-// clang-format on
 
 #endif  // LOGGER_CORE_HPP
